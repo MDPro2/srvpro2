2492eaaae4a226d869d20bc340449a84220b931c
purerosefallen <78877@qq.com>
2026-02-14 23:02:20 +0800

    add teammate surrender

--- src/room/room-module.ts
+++ src/room/room-module.ts
@@ -4,9 +4,11 @@ import { YGOProResourceLoader } from './ygopro-resource-loader';
 import { DefaultHostInfoProvider } from './default-hostinfo-provder';
 import { RoomEventRegister } from './room-event-register';
 import { RoomManager } from './room-manager';
+import { TagSurrenderConfirmMiddleware } from './tag-surrender-confirm-middleware';
 
 export const RoomModule = createAppContext<ContextState>()
   .provide(DefaultHostInfoProvider)
   .provide(YGOProResourceLoader)
   .provide(RoomManager)
+  .provide(TagSurrenderConfirmMiddleware)
   .provide(RoomEventRegister);
